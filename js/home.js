ab55c5f1818f6f3d37305ea5ef8fd81baab3474b
michiscoding <116082336+michiscoding@users.noreply.github.com>
2025-01-12 01:51:26 -0500

    added music player

--- js/home.js
+++ js/home.js
@@ -1,5 +1,12 @@
+//fade in 
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.style.opacity = '1';
+});
+
+
 window.addEventListener('load', function() {
   document.body.style.opacity = '1';
+
 });
 
 //masonry grid
@@ -14,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 //nav button
+//fade out
 function hideButton(element) {
   let opacity = 1;
   let intervalID = setInterval(function() {
@@ -26,6 +34,7 @@ function hideButton(element) {
   }, 40); 
 }
 
+//fade in
 function showButton(element) {
   let opacity = 0;
   let intervalID = setInterval(function() {
@@ -38,6 +47,7 @@ function showButton(element) {
   }, 40); 
 }
 
+//show navigation
 document.addEventListener('DOMContentLoaded', () => {
   const burger = document.getElementById('burger');
   const navMenu = document.querySelector('.nav-menu');
@@ -51,3 +61,30 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+
+//music player 
+
+//play
+function playMusic() {
+  const play = document.getElementById('play');
+  const pause = document.getElementById('pause');
+  const audio = document.getElementById('audio');
+  console.log(audio);
+
+  audio.play();
+  play.style.visibility = "hidden"
+  pause.style.visibility = "visible"
+}
+
+//pause 
+function pauseMusic() {
+  const play = document.getElementById('play');
+  const pause = document.getElementById('pause');
+  const audio = document.getElementById('audio');
+
+  audio.pause();
+  pause.style.visibility = "hidden"
+  play.style.visibility = "visible"
+}
+
